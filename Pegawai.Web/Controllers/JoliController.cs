b727a0f9a05998dae80e9cb452bcf0fce6264e22
QoirudinChris <qoirudin.christanto@gmail.com>
2016-09-23 09:40:11 +0700

    Pegawai.Web.ASP.NET Alpha 1.0

---
+++ Pegawai.Web/Controllers/JoliController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pegawai.Web.Controllers
+{
+    public class JoliController : Controller
+    {
+        // GET: Joli
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Forms()
+        {
+            return View();
+        }
+
+        public ActionResult Tables()
+        {
+            return View();
+        }
+
+        public ActionResult Latihan()
+        {
+            return View();
+        }
+
+    }
+}
